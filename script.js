e968d20ccb892c971f4b93ea2f7963ce3ed65da9
sergidl <sergidiazlopez3@gmail.com>
2022-01-27 19:13:00 +0100

    send

---
+++ script.js
@@ -0,0 +1,27 @@
+function inici() {
+    niv = document.getElementById("niv");
+
+    tag_select = document.getElementById("concurso");
+    basket = document.getElementById('basket')
+    tag_select.addEventListener("change", select_form, false);
+    let OSName = "Unknown OS";
+    if (navigator.appVersion.indexOf("Win") != -1) OSName = "Windows";
+    if (navigator.appVersion.indexOf("Mac") != -1) OSName = "MacOS";
+    if (navigator.appVersion.indexOf("X11") != -1) OSName = "UNIX";
+    if (navigator.appVersion.indexOf("Linux") != -1) OSName = "Linux";
+    if (navigator.appVersion.indexOf("Win") != -1) {
+        
+    }
+    else {
+        basket.style.display='none';
+    }
+}
+function select_form() {
+    if (tag_select.value == 'liga') {
+        niv.style.display = 'inline';
+    }
+    else {
+        niv.style.display = 'none';
+    }
+}
+window.addEventListener("load", inici, false);

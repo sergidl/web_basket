313cc1d20f8a02919f6172e4bfad69ff0c9c2276
sergidl <sergidiazlopez3@gmail.com>
2022-02-10 19:04:23 +0100

    corrections

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ function inici() {
     tag_select = document.getElementById("concurso");
     basket = document.getElementById('basket')
     tag_select.addEventListener("change", select_form, false);
-    let OSName = "Unknown OS";
-    if (navigator.appVersion.indexOf("Win") != -1) OSName = "Windows";
-    if (navigator.appVersion.indexOf("Mac") != -1) OSName = "MacOS";
-    if (navigator.appVersion.indexOf("X11") != -1) OSName = "UNIX";
-    if (navigator.appVersion.indexOf("Linux") != -1) OSName = "Linux";
-    if (navigator.appVersion.indexOf("Win") != -1) {
+    // let OSName = "Unknown OS";
+    // if (navigator.appVersion.indexOf("Win") != -1) OSName = "Windows";
+    // if (navigator.appVersion.indexOf("Mac") != -1) OSName = "MacOS";
+    // if (navigator.appVersion.indexOf("X11") != -1) OSName = "UNIX";
+    // if (navigator.appVersion.indexOf("Linux") != -1) OSName = "Linux";
+    // if (navigator.appVersion.indexOf("Win") != -1) {
         
-    }
-    else {
-        basket.style.display='none';
-    }
+    // }
+    // else {
+    //     basket.style.display='none';
+    // }
 }
 function select_form() {
     if (tag_select.value == 'liga') {
